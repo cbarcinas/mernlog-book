0b16d789a0f83beed68e52add7406e245ea2c391
Craig Barcinas <cbarcinas@live.com>
2025-06-05 15:23:17 -0700

    updated

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl-px-64';">
+        {/* Navbar */}
+        <Navbar />
+        {/* Breadscrumb */}
+        {/* Introduction */}
+        {/* featured post */}
+        {/* post list */}
+      </div>
+    </>
+  );
+}
+
+export default App;
