4f8f4a203a987d0e19bb9a17181fde417bc7f59a
Craig Barcinas <cbarcinas@live.com>
2025-06-17 17:34:44 -0700

    testing image comp

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Image } from '@imagekit/react';
+import ImageComponent from './ImageComponent';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -8,12 +8,7 @@ const Navbar = () => {
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* Logo */}
       <div className="flex items-center gap-4 text-2xl font-bold">
-        <Image
-        urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
-          src="/logo.png"
-          alt="logo"
-          className="w-8 h-8"
-        />
+        <ImageComponent src="logo.png" alt="logo" w={32} h={32} />
         <span>mernlog</span>
       </div>
       {/* Mobile Menu */}
